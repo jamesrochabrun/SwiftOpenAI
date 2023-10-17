ffbddf02129e4dc51d5f5ebdb85a2ed908bbeb27
jamesrochabrun <jamesrochabrun@gmail.com>
2023-10-16 23:07:58 -0700

    File parameter and object support

---
+++ Sources/OpenAI/Public/ResponseModels/File/FileObject.swift
@@ -0,0 +1,75 @@
+//
+//  FileObject.swift
+//
+//
+//  Created by James Rochabrun on 10/16/23.
+//
+
+import Foundation
+
+/// The [File object](https://platform.openai.com/docs/api-reference/files/object) represents a document that has been uploaded to OpenAI.
+struct FileObject: Decodable {
+   
+   /// The file identifier, which can be referenced in the API endpoints.
+   let id: String
+   /// The size of the file in bytes.
+   let bytes: Int
+   /// The Unix timestamp (in seconds) for when the file was created.
+   let createdAt: Int
+   /// The name of the file.
+   let filename: String
+   /// The object type, which is always "file".
+   let object: String
+   /// The intended purpose of the file. Currently, only "fine-tune" is supported.
+   let purpose: String
+   /// The current status of the file, which can be either uploaded, processed, pending, error, deleting or deleted.
+   let status: String
+   /// Additional details about the status of the file. If the file is in the error state, this will include a message describing the error.
+   let statusDetails: String?
+   
+   enum Status: String {
+      case uploaded
+      case processed
+      case pending
+      case error
+      case deleting
+      case deleted
+   }
+   
+   enum CodingKeys: String, CodingKey {
+       case id
+       case bytes
+       case createdAt = "created_at"
+       case filename
+       case object
+       case purpose
+       case status
+       case statusDetails = "status_details"
+   }
+   
+   init(
+      id: String,
+      bytes: Int,
+      createdAt: Int,
+      filename: String,
+      object: String,
+      purpose: String,
+      status: Status,
+      statusDetails: String?)
+   {
+      self.id = id
+      self.bytes = bytes
+      self.createdAt = createdAt
+      self.filename = filename
+      self.object = object
+      self.purpose = purpose
+      self.status = status.rawValue
+      self.statusDetails = statusDetails
+   }
+   
+   struct DeletionStatus: Decodable {
+      let id: String
+      let object: String
+      let deleted: Bool
+   }
+}

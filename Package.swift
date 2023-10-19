300cbf7bd9e31c4cc399964432a93a4ad773c5d3
jamesrochabrun <jamesrochabrun@gmail.com>
2023-10-19 00:17:18 -0700

    Renaming package

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpenAI",
+    name: "SwiftOpenAI",
     platforms: [
          .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "OpenAI",
-            targets: ["OpenAI"]),
+            name: "SwiftOpenAI",
+            targets: ["SwiftOpenAI"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "OpenAI"),
+            name: "SwiftOpenAI"),
         .testTarget(
-            name: "OpenAITests",
-            dependencies: ["OpenAI"]),
+            name: "SwiftOpenAITests",
+            dependencies: ["SwiftOpenAI"]),
     ]
 )

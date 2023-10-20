326fc5307c9507737059d3327d987efd5a998b72
jamesrochabrun <jamesrochabrun@gmail.com>
2023-10-19 21:32:09 -0700

    Initial Commit

---
+++ SwiftOpenAIExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftOpenAIExample
+//
+//  Created by James Rochabrun on 10/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftOpenAIExample/SwiftOpenAIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftOpenAIExampleApp.swift
+//  SwiftOpenAIExample
+//
+//  Created by James Rochabrun on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftOpenAIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

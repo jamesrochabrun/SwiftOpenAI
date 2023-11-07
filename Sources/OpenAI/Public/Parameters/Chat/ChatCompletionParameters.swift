5d5e4312a309f3cc8366de069c4c1833856f8817
jamesrochabrun <jamesrochabrun@gmail.com>
2023-11-06 21:59:41 -0800

    model for parallel function calls

--- Sources/OpenAI/Public/Parameters/Chat/ChatCompletionParameters.swift
+++ Sources/OpenAI/Public/Parameters/Chat/ChatCompletionParameters.swift
@@ -50,9 +50,9 @@ public struct ChatCompletionParameters: Encodable {
    
    public enum Model: String {
       case gpt35Turbo = "gpt-3.5-turbo"
-      case gpt35Turbo1006 = "gpt-3.5-turbo-1006" // Most updated
+      case gpt35Turbo1106 = "gpt-3.5-turbo-1106" // Most updated - Supports parallel function calls
       case gpt4 = "gpt-4"
-      case gpt41106Preview = "gpt-4-1106-preview"  // Most updated
+      case gpt41106Preview = "gpt-4-1106-preview"  // Most updated - Supports parallel function calls
       case gpt35Turbo0613 = "gpt-3.5-turbo-0613" // To be deprecated "2024-06-13"
       case gpt35Turbo16k0613 = "gpt-3.5-turbo-16k-0613" // To be deprecated "2024-06-13"
    }

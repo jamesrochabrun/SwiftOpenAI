b3c0cd657902b41a8a1321e3b58509b1b56e954a
jamesrochabrun <jamesrochabrun@gmail.com>
2023-11-07 01:01:47 -0800

    Updates for chat completion parameters

--- Package.swift
+++ Package.swift
@@ -14,9 +14,6 @@ let package = Package(
             name: "SwiftOpenAI",
             targets: ["SwiftOpenAI"]),
     ],
-    dependencies: [
-      .package(url: "https://github.com/jamesrochabrun/SwiftOpenAI.git", branch: "jroch-swift-open-ai-1.1")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.

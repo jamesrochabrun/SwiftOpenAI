dbcf4eeaac1347cb54e79cd4e480244d78d87b71
jamesrochabrun <jamesrochabrun@gmail.com>
2023-11-13 16:05:03 -0800

    adding error

--- Sources/OpenAI/Public/Service/OpenAIService.swift
+++ Sources/OpenAI/Public/Service/OpenAIService.swift
@@ -328,7 +328,12 @@ extension OpenAIService {
       }
       printHTTPURLResponse(httpResponse)
       guard httpResponse.statusCode == 200 else {
-         throw APIError.responseUnsuccessful(description: "status code \(httpResponse.statusCode)")
+         do {
+            let error = try decoder.decode(OpenAIErrorResponse.self, from: data)
+            throw APIError.responseUnsuccessful(description: (error.error.message ?? "") + " status code \(httpResponse.statusCode)")
+         } catch {
+            throw APIError.responseUnsuccessful(description: "status code \(httpResponse.statusCode)")
+         }
       }
       var content: [[String: Any]] = []
       if let jsonString = String(data: data, encoding: .utf8) {
@@ -358,7 +363,12 @@ extension OpenAIService {
       }
       printHTTPURLResponse(httpResponse)
       guard httpResponse.statusCode == 200 else {
-         throw APIError.responseUnsuccessful(description: "status code \(httpResponse.statusCode)")
+         do {
+            let error = try decoder.decode(OpenAIErrorResponse.self, from: data)
+            throw APIError.responseUnsuccessful(description: (error.error.message ?? "") + " status code \(httpResponse.statusCode)")
+         } catch {
+            throw APIError.responseUnsuccessful(description: "status code \(httpResponse.statusCode)")
+         }
       }
       #if DEBUG
       print("DEBUG JSON FETCH API = \(try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any])")

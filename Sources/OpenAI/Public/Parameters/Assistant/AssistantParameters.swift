4a9182abe574f266e0b087af5de9c3810320436f
jamesrochabrun <jamesrochabrun@gmail.com>
2023-11-17 16:02:03 -0800

    Adding codin keys to parameters

--- Sources/OpenAI/Public/Parameters/Assistant/AssistantParameters.swift
+++ Sources/OpenAI/Public/Parameters/Assistant/AssistantParameters.swift
@@ -12,22 +12,28 @@ import Foundation
 public struct AssistantParameters: Encodable {
    
    /// ID of the model to use. You can use the [List models](https://platform.openai.com/docs/api-reference/models/list) API to see all of your available models, or see our [Model overview](https://platform.openai.com/docs/models/overview) for descriptions of them.
-   let model: String?
+   public var model: String?
    /// The name of the assistant. The maximum length is 256 characters.
-   let name: String?
+   public var name: String?
    /// The description of the assistant. The maximum length is 512 characters.
-   let description: String?
+   public var description: String?
    /// The system instructions that the assistant uses. The maximum length is 32768 characters.
-   let instructions: String?
+   public var instructions: String?
    /// A list of tool enabled on the assistant. There can be a maximum of 128 tools per assistant. Tools can be of types code_interpreter, retrieval, or function. Defaults to []
-   let tools: [AssistantObject.Tool]?
+   public var tools: [AssistantObject.Tool]?
    /// A list of [file](https://platform.openai.com/docs/api-reference/files) IDs attached to this assistant. There can be a maximum of 20 files attached to the assistant. Files are ordered by their creation date in ascending order.
-   let fileIDS: [String]?
+   public var fileIDS: [String]?
    /// Set of 16 key-value pairs that can be attached to an object. This can be useful for storing additional information about the object in a structured format. Keys can be a maximum of 64 characters long and values can be a maxium of 512 characters long.
-   let metadata: [String: String]?
+   public var metadata: [String: String]?
    
    enum CodingKeys: String, CodingKey {
+      case model
+      case name
+      case description
+      case instructions
+      case tools
       case fileIDS = "file_ids"
+      case metadata
    }
    
    public enum Action {

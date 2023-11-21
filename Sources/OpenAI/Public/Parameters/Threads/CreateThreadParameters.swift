c28ad14f20dd78b4311c59b9607a5eb97f5da846
jamesrochabrun <jamesrochabrun@gmail.com>
2023-11-21 00:54:24 -0800

    Threads initializer

--- Sources/OpenAI/Public/Parameters/Threads/CreateThreadParameters.swift
+++ Sources/OpenAI/Public/Parameters/Threads/CreateThreadParameters.swift
@@ -11,8 +11,16 @@ import Foundation
 public struct CreateThreadParameters: Encodable {
    
    /// A list of [messages](https://platform.openai.com/docs/api-reference/messages) to start the thread with.
-   let messages: [MessageObject]
+   public var messages: [MessageObject]?
    
    /// Set of 16 key-value pairs that can be attached to an object. This can be useful for storing additional information about the object in a structured format. Keys can be a maximum of 64 characters long and values can be a maxium of 512 characters long.
-   let metadata: [String: String]
+   public var metadata: [String: String]?
+   
+   public init(
+      messages: [MessageObject]? = nil,
+      metadata: [String : String]? = nil)
+   {
+      self.messages = messages
+      self.metadata = metadata
+   }
 }

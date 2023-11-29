37f0383b4eee1b78ed93850672f1b8b3713db059
jamesrochabrun <jamesrochabrun@gmail.com>
2023-11-29 00:25:16 -0800

    Making metadata optional

--- Sources/OpenAI/Public/ResponseModels/Runs/RunStepObject.swift
+++ Sources/OpenAI/Public/ResponseModels/Runs/RunStepObject.swift
@@ -39,7 +39,7 @@ public struct RunStepObject: Decodable {
    /// The Unix timestamp (in seconds) for when the run step completed.
    public let completedAt: Int?
    /// Set of 16 key-value pairs that can be attached to an object. This can be useful for storing additional information about the object in a structured format. Keys can be a maximum of 64 characters long and values can be a maxium of 512 characters long.
-   public let metadata: [String: String]
+   public let metadata: [String: String]?
    
    public enum Status: String {
       case inProgress = "in_progress"

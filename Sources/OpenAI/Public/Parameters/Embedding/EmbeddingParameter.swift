e5b3ca8939724a30fb7a661dda6ae2b98b63ad92
Michel-André Chirita <macistador@hotmail.com>
2024-12-30 13:02:50 +0100

    feat: adds new embeddings models

--- Sources/OpenAI/Public/Parameters/Embedding/EmbeddingParameter.swift
+++ Sources/OpenAI/Public/Parameters/Embedding/EmbeddingParameter.swift
@@ -24,6 +24,8 @@ public struct EmbeddingParameter: Encodable {
    
    public enum Model: String {
       case textEmbeddingAda002 = "text-embedding-ada-002"
+      case textEmbedding3Large = "text-embedding-3-large"
+      case textEmbedding3Small = "text-embedding-3-small"
    }
    
    enum CodingKeys: String, CodingKey {

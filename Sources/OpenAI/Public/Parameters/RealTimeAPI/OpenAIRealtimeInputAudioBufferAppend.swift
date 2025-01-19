c7aca648a4f912569be183c1152aabab117a0e3f
jamesrochabrun <jamesrochabrun@gmail.com>
2025-01-18 22:30:27 -0800

    Adding Audio buffer

--- Sources/OpenAI/Public/Parameters/RealTimeAPI/OpenAIRealtimeInputAudioBufferAppend.swift
+++ Sources/OpenAI/Public/Parameters/RealTimeAPI/OpenAIRealtimeInputAudioBufferAppend.swift
@@ -1,8 +1,21 @@
 //
-//  File.swift
+//  OpenAIRealtimeInputAudioBufferAppend.swift
 //  SwiftOpenAI
 //
 //  Created by James Rochabrun on 1/18/25.
 //
 
 import Foundation
+
+
+public struct OpenAIRealtimeInputAudioBufferAppend: Encodable {
+   
+   public let type = "input_audio_buffer.append"
+   
+   /// base64 encoded PCM16 data
+   public let audio: String
+   
+   public init(audio: String) {
+      self.audio = audio
+   }
+}

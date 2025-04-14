4fe3a5a498c0f0535f68a00fe1589eda30c3721a
Xinghua <flexih@gmail.com>
2025-04-14 18:50:04 +0800

    use encodeIfPresent instead

--- Sources/OpenAI/Public/Parameters/Chat/ChatCompletionParameters.swift
+++ Sources/OpenAI/Public/Parameters/Chat/ChatCompletionParameters.swift
@@ -148,9 +148,7 @@ public struct ChatCompletionParameters: Encodable {
                public func encode(to encoder: Encoder) throws {
                   var container = encoder.container(keyedBy: CodingKeys.self)
                   try container.encode(url, forKey: .url)
-                  if let detail, !detail.isEmpty {
-                     try container.encode(detail, forKey: .detail)
-                  }
+                  try container.encodeIfPresent(detail, forKey: .detail)
                }
                
                public init(url: URL, detail: String? = nil) {
